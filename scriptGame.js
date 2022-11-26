7e879268cb677a1e5178c318ee4d377d3f3a98e1
valentine974 <valentine.bathany@hotmail.fr>
2022-11-26 10:25:39 +0100

    test

--- scriptGame.js
+++ scriptGame.js
@@ -1,12 +1,15 @@
+const myBlocks = [];
+
 const gameLandscape = {
   canvas: document.createElement("canvas"),
+  frames : 0,
 
   start: function () {
     this.canvas.width = 800;
     this.canvas.height = 400;
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    this.interval = setInterval(updateGameArea, 15);
+    this.interval = setInterval(updateGameArea, 20);
   },
   clear: function () {
     landscapeImage.move();
@@ -15,6 +18,8 @@ const gameLandscape = {
   },
 };
 
+const gravity = 0.5;
+
 class ghibliChar {
   constructor(image, x, y, width, height) {
     this.width = width;
@@ -22,7 +27,7 @@ class ghibliChar {
     this.x = x;
     this.y = y;
     this.speedX = 0;
-    this.speedY = 0;
+    this.speedY = 1;
     this.img = image;
   }
 
@@ -34,112 +39,163 @@ class ghibliChar {
   newPos() {
     this.x += this.speedX;
     this.y += this.speedY;
+    if(this.y + this.height + this.speedY <245+this.height) { this.speedY += gravity}
+    else { this.speedY = 0}
   }
+  left() {
+    return this.x;
+  }
+  right() {
+    return this.x + this.width;
+  }
+  top() {
+    return this.y;
+  }
+  bottom() {
+    return this.y + this.height;
+  }
+}
+
+
+class Blocks {
+  constructor(color, x, y, width, height) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y; 
+    this.color = color;
+  }
+
+  update() {
+    const ctx = gameLandscape.context; 
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+ 
 }
 
 function updateGameArea() {
   gameLandscape.clear();
   susuwatari.newPos();
-  susuwatari.update();
-}
-
-
-
+  susuwatari.update(); 
+  updateCalcifersImage()
+  // updateBlocksImage() 
+  // updateBlocks(); 
+} 
 
 const trainPartOne = new Image();
-trainPartOne.src = "./Pictures/Train11.png";
-
-const trainPartTwo = new Image();
-trainPartTwo.src = "./Pictures/Train33.png";
-
-const trainPartThree = new Image();
-trainPartThree.src = "./Pictures/Train44.png";
+trainPartOne.src = "./Pictures/Train final.png"; 
 
 const landscapeImage = {
-  img1: trainPartOne,
-  img1height: 400,
-  img1width: 2220,
-  img2: trainPartTwo,
-  img2height: 400,
-  img2width: 3040,
-  img3: trainPartThree,
-  img3height: 400,
-  img3width: 2240,
-  x1: 0,
-  x2: 0,
-  x3: 0,
-  speed: -0.7, // input the player's speed to moove the background Image
+  img: trainPartOne,
+  height: 400,
+  width: 3680, 
+  x: 0, 
+  speed: 0, // input the player's speed to moove the background Image
   canvas: gameLandscape.canvas,
 
   move: function () {
-    this.x1 += this.speed;
-    this.x2 = this.x1 + this.img1width + this.speed;
-    this.x3 = this.x2 + this.img2width;
-    susuwatari.speedX-=0.002
+    // susuwatari.speedX-=0.002
+    if(this.x >0 ){this.x = 0 }  
+    else if (this.x + this.width + this.speed < this.canvas.width){this.speed=0;} 
+    this.x += this.speed; 
   },
 
   draw: function () {
     const ctx = gameLandscape.context;
-    ctx.drawImage(this.img1, this.x1, 0, this.img1width, this.img1height);
-    ctx.drawImage(this.img2, this.x2, 0, this.img2width, this.img2height);
-    ctx.drawImage(this.img3, this.x3, 0, this.img3width, this.img3height);
+    ctx.drawImage(this.img, this.x, 0, this.width, this.height); 
   },
 };
 
+// function updateBlocksImage() { 
+//  for (i = 0; i < myBlocks.length; i++) {  
+//    myBlocks[i].update();    
+//  } 
+ 
+// }
 
+function updateCalcifersImage() { 
+ for (i = 0; i < myBlocks.length; i++) {  
+  myBlocks[i].newPos(); 
+   myBlocks[i].update();    
+ } 
+ 
+}
 
 const susuImage = new Image();
 susuImage.src = "./Pictures/SusuwatariV2.png";
+const calciferImage = new Image();
+calciferImage.src = "./Pictures/calcifer.gif";
 
-const susuwatari = new ghibliChar(susuImage, 100, 240, 80, 65);
+const susuwatari = new ghibliChar(susuImage, 160, 245, 80, 65);
 
 gameLandscape.start();
 
-function jump (v) {
-  susuwatari.speedY += v ;
-  susuwatari.speedX +=0.5;
-  landscapeImage.speed = -3;
+function updateCalcifers (){
+  let nextBlockX = gameLandscape.canvas.width
+  for (i = 0; i < myBlocks.length; i++) { 
+    myBlocks[i].x -= 30; 
+    myBlocks[i].update();   
+    nextBlockX = myBlocks[i].x +300
+  }
+
+    let y = 0
+    let x = nextBlockX;
+    let width = 50;
+    let height = 50;   
+    myBlocks.push(new ghibliChar(calciferImage, x, y, width, height)); 
 
 }
 
+
+// function updateBlocks() {// utiliser for i until 5  pour créer un nombre de plateformes limité 
+//    let nextBlockX = gameLandscape.canvas.width
+//   for (i = 0; i < myBlocks.length; i++) { 
+//     myBlocks[i].x -= 30;
+//     myBlocks[i].update();   
+//     nextBlockX = myBlocks[i].x +300
+//   }
+//     let y
+//     if(myBlocks.length%2===0){ y= 250}
+//     else {y = 150} 
+//     let x = nextBlockX;
+//     let width = 150;
+//     let height = 30;   
+//     myBlocks.push(new Blocks('black', x , y, width,height)); 
+  
+// }
+
 document.addEventListener("keydown", (e) => {
   switch (e.keyCode) {
-    case 38: // up arrow to make the Susuwatari jump
-
-      jump (-3)  ;
-      setTimeout(() => { jump (-4) }, 50); 
-      setTimeout(() => { jump (-4)}, 100); 
-      setTimeout(() => { jump (-5)}, 150);
-      setTimeout(() => { jump (5) }, 200);
-      setTimeout(() => { jump (4) }, 250);
-      setTimeout(() => { jump (4) }, 300);
-      setTimeout(() => { jump (3) }, 350);
-
-      setTimeout(() => {
-        susuwatari.speedY = 0;
-        susuwatari.speedX = 0;  
-        landscapeImage.speed = -0.7;
-        susuwatari.y = 240 ; 
-        console.log("9")
-      }, 400);
+    case 38: // up arrow to make the Susuwatari 
+    susuwatari.speedY -= 20; 
+    susuwatari.speedX += 2;
+    landscapeImage.speed = -10; 
+    
+    updateCalcifers()
+    // updateBlocks()
 
       break;
 
     case 37: // left arrow
       susuwatari.speedX -= 1;
-      landscapeImage.speed = -0.7;
+      landscapeImage.speed += 10;
       break;
     case 39: // right arrow
       susuwatari.speedX += 1;
-      landscapeImage.speed = -2;
+      landscapeImage.speed = -10;
+      // updateBlocks()
+
+  updateCalcifers()
+      
       break;
   }
 });
 document.addEventListener("keyup", (e) => {
   susuwatari.speedX = 0;
   susuwatari.speedY = 0;
-  landscapeImage.speed = -0.7;
+  landscapeImage.speed = 0;
 });
 
 
-console.log(document.querySelector('button .start'))
+
